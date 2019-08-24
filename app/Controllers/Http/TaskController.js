2506b38e73e5bf86b34ee8f1bdc4900b601f4b9a
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-08-24 20:01:23 -0300

    crud tasks

--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -7,6 +7,7 @@
 /**
  * Resourceful controller for interacting with tasks
  */
+const Task = use('App/Models/Task')
 class TaskController {
   /**
    * Show a list of all tasks.
@@ -17,19 +18,13 @@ class TaskController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
+  async index ({ params, request, response, view }) {
+    const tasks = await Task
+                          .query()
+                          .where('project_id', params.projects_id)
+                          .fetch()
 
-  /**
-   * Render a form to be used for creating a new task.
-   * GET tasks/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    return tasks
   }
 
   /**
@@ -40,7 +35,17 @@ class TaskController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ params, request, response }) {
+    const data = request.only([
+      'user_id',
+      'file_id',
+      'title',
+      'due_date',
+      'description',
+    ])
+
+    const task = await Task.create({ ...data, project_id: params.projects_id })
+    return task
   }
 
   /**
@@ -53,18 +58,8 @@ class TaskController {
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing task.
-   * GET tasks/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+    const task = await Task.findOrFail(params.id)
+    return task;
   }
 
   /**
@@ -76,6 +71,17 @@ class TaskController {
    * @param {Response} ctx.response
    */
   async update ({ params, request, response }) {
+    const task = await Task.findOrFail(params.id)
+    const data = request.only([
+      'user_id',
+      'file_id',
+      'title',
+      'due_date',
+      'description',
+    ])
+    task.merge(data);
+    await task.save()
+    return task;
   }
 
   /**
@@ -87,6 +93,8 @@ class TaskController {
    * @param {Response} ctx.response
    */
   async destroy ({ params, request, response }) {
+    const task = await Task.findOrFail(params.id)
+    await task.delete()
   }
 }
 
