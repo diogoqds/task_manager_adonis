7c1b4b2a367712d41215fda38ccf144c8ee2bdb6
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-08-24 20:09:31 -0300

    install validator

--- start/routes.js
+++ start/routes.js
@@ -16,7 +16,8 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.post('users', 'UserController.store')
+Route.post('users', 'UserController.store').validator('User')
+
 Route.post('sessions', 'SessionController.store')
 
 Route.post('forgot_password', 'ForgotPasswordController.store')
@@ -25,6 +26,7 @@ Route.put('forgot_password', 'ForgotPasswordController.update')
 
 Route.group(() => {
   Route.post('files', 'FileController.store')
+
   Route.get('files/:id', 'FileController.show')
   Route.resource('projects','ProjectController').apiOnly()
   Route.resource('projects.tasks','TaskController').apiOnly()
