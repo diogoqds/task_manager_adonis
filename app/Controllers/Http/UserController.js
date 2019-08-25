5a3df91f934677e784c0629fdbb64b63c6982b76
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-08-25 12:18:15 -0300

    create transaction

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,13 +1,21 @@
 'use strict'
 
 const User = use('App/Models/User');
+const Database = use('Database')
 
 class UserController {
 
   async store({ request }) {
     const userParams = request.only(['username', 'password', 'email']);
-    const user = await User.create(userParams);
+    const addressParams = request.input('addresses')
 
+    const trx = await Database.beginTransaction()
+
+    const user = await User.create(userParams, trx);
+
+    await user.addresses().createMany(addressParams, trx)
+
+    await trx.commit()
     return user;
   }
 }
---
+++ database/migrations/1566745893557_address_schema.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class AddressSchema extends Schema {
+  up () {
+    this.create('addresses', (table) => {
+      table.increments()
+      table
+          .integer('user_id')
+          .unsigned()
+          .references('id')
+          .inTable('users')
+      table.string('street').notNullable()
+      table.string('number').notNullable()
+      table.string('city').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('addresses')
+  }
+}
+
+module.exports = AddressSchema
